12b79afd7b8cc91e19ce88f95d5df8c62e5abe18
govind-codex <nagargovind902@gmail.com>
2026-05-18 14:17:58 +0530

    changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const cookieParser = require("cookie-parser")
 const authRouter = require('./routes/auth.route');
+const cors = require('cors');
 
 
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: 'http://localhost:5173', // Frontend URL
+  credentials: true, // Allow cookies to be sent
+}));
 
 
 app.use('/api/auth', authRouter);
