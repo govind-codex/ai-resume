8eab2d921803f4c4588e9d5e10c925aa279ac4d8
govind-codex <nagargovind902@gmail.com>
2026-05-18 12:05:32 +0530

    manage state & api through hook layer

--- Frontend/src/features/auth/hooks/useAuth.js
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -1,5 +1,22 @@
 import { useContext } from "react";
 import { AuthContext } from "../auth.context";
+import { login , register, logout, getMe} from "../services/auth.api";
 
-const context = useContext(AuthContext)
-const {user, setUser, loading, setloading} = context
+export const useAuth = () => {
+  const context = useContext(AuthContext)
+  const {user, setUser, loading, setloading} = context
+  const handleLogin = async ({ email, password }) => {
+    setloading(true)
+    const data = await login({ email, password })
+    setUser(data.user)
+    setloading(false)
+  }
+
+  export const handleRegister = async ({ username , email,  password }) => {
+    setloading(true)
+    const data = await register({ username, email, password})
+    setUser(data.user)
+    setloading(false)
+  }
+
+}
