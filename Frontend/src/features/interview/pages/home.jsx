c24e95914ed5c7c8b6a8476d14ff66251c0e1132
govind-codex <nagargovind902@gmail.com>
2026-06-09 15:36:26 +0530

    createing ui for user after login

--- Frontend/src/features/interview/pages/home.jsx
+++ Frontend/src/features/interview/pages/home.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
-import "..style/home.scss"
+import "../style/home.scss"
 
 const Home = () => {
   return (
-    <main className='home'>  
-        <div className="left">
-            <textarea name="jobDescription" id="jobDescription" placeholder='Enter job Description Here....'></textarea>
-        </div>
-        <div className="right">
-            <div className="input-group">
-                <label htmlFor="resume" >Upload Resume</label>
-                <input type="file" name="resume" id="resume" accept='.pdf' />
+    <main className='home'>
+        <div className="interview-input-group">  
+            <div className="left">
+                <label htmlFor="jobDescription">Job Description</label>
+                <textarea name="jobDescription" id="jobDescription" placeholder='Enter job Description Here....'></textarea>
             </div>
-            <div className="input-group">
-                <label htmlFor="selfDescription" >Enter Self Description</label>
-                <textarea name="selfDescription" id="selfDescription" placeholder='Enter self Description Here....'></textarea>
+            <div className="right">
+                <div className="input-group">
+                    <p>Resume <small className="highlight">(Use resume and self description together for best results.)</small></p>
+                    <label className="file-label" htmlFor="resume">Upload Resume</label>
+                    <input hidden type="file" name="resume" id="resume" accept='.pdf' />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="selfDescription" >Enter Self Description</label>
+                    <textarea name="selfDescription" id="selfDescription" placeholder='Enter self Description Here....'></textarea>
+                </div>
+                <button className='button primary-button'>Generate Interview Report</button>
             </div>
-            <button className='generate-btn'>Generate Interview Report</button>
         </div>
     </main>
   )
